5858c65502a29d653de1ad32881f8402fcc199d5
TMillsap04 <tmmillsap@student.fullsail.edu>
2023-04-12 15:46:58 -0500

    Creating Schemas for database.

--- ecommerce/ecommerce/sanityecommerce/schemas/product.js
+++ ecommerce/ecommerce/sanityecommerce/schemas/product.js
@@ -1,4 +1,11 @@
 export default {
-    name: 'product',
-    title: 'Product',
+  name: 'product',
+  title: 'Product',
+  type: 'document',
+    fields: [{
+        name: 'image',
+        title: 'Image',
+        type: 'array',
+        of: [{type: 'image'}]
+    }]
 }
