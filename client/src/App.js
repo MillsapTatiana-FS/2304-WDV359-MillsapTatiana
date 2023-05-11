57b88f074e29662a8435f514e086c74dbbb6fd59
TMillsap04 <tmmillsap@student.fullsail.edu>
2023-05-11 17:26:42 -0500

    Tying up loose ends.

---
+++ client/src/App.js
@@ -0,0 +1,21 @@
+import { useEffect }  from "react";
+import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
+
+const ScrollToTop = () => {
+  const { pathname } = useLocation();
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+  return null;
+
+};
+
+function App() {
+  return (
+    <div className="app">
+      
+    </div>
+  );
+}
+
+export default App;
