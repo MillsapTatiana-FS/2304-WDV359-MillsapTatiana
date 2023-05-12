0c576b037bd3cf236de6458db487a6997b41770b
TMillsap04 <tmmillsap@student.fullsail.edu>
2023-05-11 20:05:07 -0500

    Working on loading items.

--- client/src/scenes/global/Footer.jsx
+++ client/src/scenes/global/Footer.jsx
@@ -24,7 +24,7 @@ function Footer() {
             mb="30px"
             color={shades.secondary[500]}
           >
-            ECOMMER
+            Astrum
           </Typography>
           <div>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
